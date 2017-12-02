1a4cf616399cb318d7917e23f18a51442de7b18a
migonzalez64 <migonzalez64@outlook.com>
2017-12-02 12:48:48 -0800

    Add files via upload

---
+++ spec/now/playing_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Now::Playing do
+  it "has a version number" do
+    expect(Now::Playing::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
